ecafdff1e43e9684d090a98a5664d0c92830d33b
Scott Thomas <sthomas6505@gmail.com>
2021-11-30 22:23:15 +0000

    Extended base html files

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
     details = models.TextField()
-    url = models.URLField(blank=True)
+
+    def __str__(self):      # Showss the title in the admin pages.
+        return self.title
